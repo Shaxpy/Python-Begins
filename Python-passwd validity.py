ad54f1ba2404c58b938dad583500206d7b2b5ecf
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2019-01-04 21:52:37 +0530

    Update Python-passwd validity.py

--- Python-passwd validity.py
+++ Python-passwd validity.py
@@ -5,32 +5,31 @@ Created on Fri Jan  4 20:02:03 2019
 @author: SHASHWAT PANDEY
 """
 
-import re
-password="backstreet boys"
-t=0
-while true:
-    if (len(password)<8): 
-        flag = -1
+import re 
+password = input()
+t = 0
+while True:
+    if not re.search("[a-z]",password):
+        t=1     
         break
-    elif not re.search("[a-z]", password): 
-        flag = -1
+    elif not re.search("[0-9]",password):
+        t=1     
         break
-    elif not re.search("[A-Z]", password): 
-        flag = -1
+    elif not re.search("[A-Z]",password):
+        t=1     
         break
-    elif not re.search("[0-9]", password): 
-        flag = -1
+    elif not re.search("[$#@]",password):
+        t=1     
         break
-    elif not re.search("[_@$]", password): 
-        flag = -1
+    elif not (len(password)<6):
+        t=1     
         break
-    elif re.search("\s", password): 
-        flag = -1
+    elif not (len(password)>12):
+        t=1     
         break
-    else: 
-        flag = 0
-        print("Valid Password") 
-        break
-  
-if flag ==-1: 
-    print("Not a Valid Password")
+    else:
+        t=0
+        print("Valid Password")
+if t==1:
+    print("Invalid Password")
+        
